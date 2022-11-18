152d1eb04b272507f079038f2b8fdd9510093b86
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2022-11-18 10:49:38 +0100

    Add sign_latest_release.sh

---
+++ sign_latest_release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+tmp_dir="$(mktemp -d)"
+wget -q -O "$tmp_dir/secrets.tar.gz" https://github.com/theCalcaholic/nextcloud-secrets/releases/latest/download/secrets.tar.gz
+wget -q -O "$tmp_dir/secrets.sha256" https://github.com/theCalcaholic/nextcloud-secrets/releases/latest/download/secrets.sha256
+
+checksum="$(cd "$tmp_dir"; sha256sum "secrets.tar.gz")"
+[[ "$checksum" == "$(cat "$tmp_dir/secrets.sha256")" ]] || {
+	echo "Checksum mismatch!" >&2
+	echo "Expected: $(cat "$tmp_dir/secrets.sha256")"
+	echo "Was:      $checksum"
+	exit 1
+}
+
+openssl dgst -sha512 -sign "$HOME/.nextcloud/certificates/secrets.key" "$tmp_dir/secrets.tar.gz" | openssl base64
+
