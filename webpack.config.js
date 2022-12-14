8b7073f520d667d45b2d29e1618d648d37238d4d
Vitor Mattos <vitor@php.rio>
2022-12-14 16:55:54 -0300

    Render public page as base template without menu
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 // SPDX-FileCopyrightText: Tobias Knöppler <thecalcaholic@web.de>
 // SPDX-License-Identifier: AGPL-3.0-or-later
+const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+
+webpackConfig.entry = {
+	public: path.resolve(path.join('src', 'public.js')),
+	main: path.resolve(path.join('src', 'main.js')),
+}
 
 module.exports = webpackConfig
