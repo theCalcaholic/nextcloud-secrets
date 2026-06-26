50d5eaadf70550a015613c95d618ee6281a90d3d
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2026-06-26 12:25:20 +0200

    setup.ts: Fix installation of notifications app on stable34

--- playwright/support/setup.ts
+++ playwright/support/setup.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { configureNextcloud, runOcc } from '@nextcloud/e2e-test-server'
+import { configureNextcloud, getContainer, runExec, runOcc } from '@nextcloud/e2e-test-server'
 import { test as setup } from '@playwright/test'
 import path from 'node:path'
 import { getBranch, syncApp } from '../util.ts'
@@ -15,11 +15,26 @@ import { getBranch, syncApp } from '../util.ts'
  * as that only checks for the URL to be accessible which happens already before everything is configured.
  */
 setup('Configure Nextcloud', async () => {
+	const ncVersion = process.env.NC_VERSION ?? getBranch()
 	const cwd = process.cwd()
 	const syncPath = path.join(cwd, 'build/test/secrets')
 	await syncApp(cwd, syncPath, [path.join(cwd, 'build'), path.join(cwd, '.git'), path.join(cwd, '.pnpm'), path.join(cwd, 'node_modules')])
-	const appsToInstall: string[] = ['viewer', 'notifications', 'activity']
-	await configureNextcloud(appsToInstall, process.env.NC_VERSION ?? getBranch())
+	const appsToInstall: string[] = ['viewer', 'activity'] // TODO: Readd notifications
+	await configureNextcloud(appsToInstall, ncVersion)
+
+	// Fix for notifications on stable34 missing vendor directory
+	// TODO: Remove once fixed
+	const container = await getContainer()
+	const repo = ncVersion === 'stable34' ? 'https://github.com/theCalcaholic/nc-notifications.git' : 'https://github.com/nextcloud/notifications.git'
+	await runExec(
+		['git', 'clone', '--depth=1', `--branch=${ncVersion}`, repo, 'apps/notifications'],
+		{
+			container,
+			verbose: true,
+		},
+	)
+	await runOcc(['app:enable', '--force', 'notifications'], { container, verbose: true })
+
 	await runOcc(['app:enable', 'secrets'])
 	await runOcc(['user:add', '--password-from-env', 'secretsclitest'], {
 		env: ['NC_PASS=secretsclitest'],
