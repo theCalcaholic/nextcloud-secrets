7cd00b29f0403dfe338909f90e6bfe8ae997a064
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2026-07-17 14:36:51 +0200

    setup.ts: Add workaround for broken notifications app

--- playwright/support/setup.ts
+++ playwright/support/setup.ts
@@ -5,6 +5,9 @@
 
 import { configureNextcloud, getContainer, runExec, runOcc } from '@nextcloud/e2e-test-server'
 import { test as setup } from '@playwright/test'
+import { execSync } from 'node:child_process'
+import { existsSync, mkdirSync, rmSync } from 'node:fs'
+import { tmpdir } from 'node:os'
 import path from 'node:path'
 import { getBranch, syncApp } from '../util.ts'
 
@@ -15,6 +18,7 @@ import { getBranch, syncApp } from '../util.ts'
  * as that only checks for the URL to be accessible which happens already before everything is configured.
  */
 setup('Configure Nextcloud', async () => {
+	setup.setTimeout(300_000)
 	const ncVersion = process.env.NC_VERSION ?? getBranch()
 	const cwd = process.cwd()
 	const syncPath = path.join(cwd, 'build/test/secrets')
@@ -22,18 +26,29 @@ setup('Configure Nextcloud', async () => {
 	const appsToInstall: string[] = ['viewer', 'activity'] // TODO: Readd notifications
 	await configureNextcloud(appsToInstall, ncVersion)
 
-	// Fix for notifications on stable34 missing vendor directory
+	await runExec(['bash', '-c', 'cd apps/notifications && composer install minishlink/web-push'])
+
+	// Fix for notifications on stable34 broken/missing dependencies
 	// TODO: Remove once fixed
 	const container = await getContainer()
-	const repo = ncVersion === 'stable34' ? 'https://github.com/theCalcaholic/nc-notifications.git' : 'https://github.com/nextcloud/notifications.git'
-	await runExec(
-		['git', 'clone', '--depth=1', `--branch=${ncVersion}`, repo, 'apps/notifications'],
-		{
-			container,
-			verbose: true,
-		},
-	)
-	await runOcc(['app:enable', '--force', 'notifications'], { container, verbose: true })
+
+	const coreApps = ['notifications']
+
+	const tmpDir = tmpdir() + '/' + crypto.randomUUID()
+	mkdirSync(tmpDir)
+	for (const app of coreApps) {
+		console.log(`installing ${app} ...`)
+		const repo = `https://github.com/nextcloud/${app}`
+		if (!existsSync(`${tmpDir}/${app}`)) {
+			execSync(`git clone  --branch=${ncVersion} --depth=1 ${repo} ${tmpDir}/${app}`)
+		}
+		execSync('composer install', { cwd: `${tmpDir}/${app}` })
+		execSync(`tar -caf ${app}.tar.gz ${app}`, { cwd: `${tmpDir}` })
+		await runExec(['rm', '-rf', `apps/${app}`], { verbose: true, container })
+		await container.putArchive(`${tmpDir}/${app}.tar.gz`, { path: '/var/www/html/apps/' })
+		await runOcc(['app:enable', '--force', app], { container, verbose: true })
+	}
+	rmSync(tmpDir, { force: true, recursive: true })
 
 	await runOcc(['app:enable', 'secrets'])
 	await runOcc(['user:add', '--password-from-env', 'secretsclitest'], {
